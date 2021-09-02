2a2dbf485cdfc8295b5d0e0ea7095677216c5609
Erik Persson <erikp8@kth.se>
2021-09-02 14:41:00 +0200

    add libinput-gestures and screenlocker to autostart.sh

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -10,8 +10,8 @@ nitrogen --restore &
 dunst &
 
 # Start screenlocker
-# xset s 300 5
-# xss-lock -n /usr/lib/xsecurelock/dimmer -l -- xsecurelock &
+xset s 300 5
+xss-lock -n /usr/lib/xsecurelock/dimmer -l -- xsecurelock &
 
 # Authenticathon manager
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
@@ -23,3 +23,8 @@ xinput --set-prop 'pointer:Razer Razer DeathAdder 2013' 'libinput Accel Profile 
 xrandr --output HDMI-0 --mode 1920x1080 --rate 60 --right-of DP-0
 xrandr --output DVI-D-0 --mode 1920x1080 --rate 60 --right-of DP-0
 xrandr --output DP-0 --mode 1920x1080 --rate 144 --primary
+
+if command -v libinput-gestures &> /dev/null
+then
+	libinput-gestures &
+fi
